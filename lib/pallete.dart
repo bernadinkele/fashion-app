b79246eb54bf57da68f2d200b9ee85e39df2af8f
bernadinkele <kelebernadin00@gmail.com>
2023-09-12 09:42:03 +0000

    initialisation du projet

---
+++ lib/pallete.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Pallete{
+  static const whilteColor=Color(0xFFFFFFFF);
+  static const grayColor=Color(0xFFCFCFCF);
+  static const blackColor=Color(0xFF000000);
+  static const blueColor=Color(0xFF000BE1);
+
+
+
+}
